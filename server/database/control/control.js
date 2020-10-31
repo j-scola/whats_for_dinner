eb05e965fb00769009b4154d17c7ae3e3a221c9b
James Scolamieri <james.scolamieri@gmail.com>
2020-10-31 11:17:28 -0700

    adds post route integration with db, creates function for delete route

--- server/database/control/control.js
+++ server/database/control/control.js
@@ -30,4 +30,20 @@ module.exports = {
       res.send(500);
     }
   },
+  removeIngredient: (req, res) => {
+    console.log(req.body);
+    if (req.body.name) {
+      model.addIngredient(req.body, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.send(500);
+        } else {
+          res.status(200).send(data);
+        }
+      });
+    } else {
+      console.log('request body format incorrect');
+      res.send(500);
+    }
+  },
 };
--- server/database/model/model.js
+++ server/database/model/model.js
@@ -1,10 +1,14 @@
-const db = require('../index.js');
+const Ingredient = require('../index.js');
 
 module.exports = {
   getIngredients: (cb) => {
-    db.find({}, cb);
+    Ingredient.find({}, cb);
   },
   addIngredient: (info, cb) => {
-    db.insertOne(info, cb);
+    const item = new Ingredient(info);
+    item.save({ validateBeforeSave: true }, cb);
+  },
+  removeIngredient: (info, cb) => {
+    Ingredient.findOneAndRemove(info, cb);
   },
 };
